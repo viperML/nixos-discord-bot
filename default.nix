758261f9ba92ef898cbaeee03bf0c8d288bc9b60
Fernando Ayats <ayatsfer@gmail.com>
2023-12-11 12:13:33 +0000

    tweak shell

--- default.nix
+++ default.nix
@@ -1,3 +1,4 @@
-{ pkgs ? import <nixpkgs> {}}: {
+{ pkgs ? import <nixpkgs> {}}: rec {
   package = pkgs.python3.pkgs.callPackage ./package.nix {};
+  dev-package = package.overrideAttrs (_: {src=null;});
 }
